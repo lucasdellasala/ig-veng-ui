c081cfea011751441a02b8b3f89a9c21f81260ae
lucasdellasala <lucasdellasala@gmail.com>
2023-12-05 23:44:05 -0300

    change more shit

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
+// Importa las librerías necesarias
 import React, { useState, ChangeEvent, useEffect } from 'react';
 import './App.css';
 
+// Interfaces de usuario y componentes relacionados
 interface User {
   id: string;
   username: string;
@@ -34,9 +36,11 @@ interface CardProps {
   user: User;
 }
 
+// URLs para imágenes por defecto
 const defaultProfilePic = 'https://cdn4.iconfinder.com/data/icons/instagram-ui-twotone/48/Paul-18-512.png';
 const verificationIcon = 'https://www.pngitem.com/pimgs/m/302-3024199_instagram-verified-symbol-png-instagram-verified-logo-png.png';
 
+// Componente de tarjeta de usuario
 const Card: React.FC<CardProps> = ({ user }) => {
   const handleImageError = (event: React.SyntheticEvent<HTMLImageElement, Event>) => {
     const img = event.target as HTMLImageElement;
@@ -54,31 +58,39 @@ const Card: React.FC<CardProps> = ({ user }) => {
         />
       </div>
       <div className="user-info">
-        <a target="_blank" href={`https://instagram.com/${user.username}`}  rel="noopener noreferrer"><h2>
-          {user.full_name}{' '}
-          {user.is_verified && (
-            <img
-              className="verification-icon"
-              src={verificationIcon}
-              alt="Verified"
-              title="Verified"
-            />
-          )}
-        </h2></a>
-        <a target="_blank" href={`https://instagram.com/${user.username}`}  rel="noopener noreferrer"><p>@{user.username}</p></a>
-        <p>{user.is_private ? 'Private' : 'Public'}</p>
+        <a target="_blank" href={`https://instagram.com/${user.username}`} rel="noopener noreferrer">
+          <h2>
+            {user.full_name}{' '}
+            {user.is_verified && (
+              <img
+                className="verification-icon"
+                src={verificationIcon}
+                alt="Verified"
+                title="Verified"
+              />
+            )}
+          </h2>
+        </a>
+        <a target="_blank" href={`https://instagram.com/${user.username}`} rel="noopener noreferrer">
+          <p>@{user.username}</p>
+        </a>
+        <p>{user.is_private ? 'Privado' : 'Público'}</p>
       </div>
     </div>
   );
 };
 
+// Número de elementos por página
 const ITEMS_PER_PAGE = 12;
 
+// Componente principal de la aplicación
 const App: React.FC = () => {
   const [currentPage, setCurrentPage] = useState(1);
   const [usersData, setUsersData] = useState<User[]>([]);
   const [fileLoaded, setFileLoaded] = useState(false);
+  const [showVerified, setShowVerified] = useState(true);
 
+  // Efecto para cargar datos almacenados localmente
   useEffect(() => {
     const storedData = localStorage.getItem('usersData');
     if (storedData) {
@@ -92,6 +104,7 @@ const App: React.FC = () => {
     }
   }, []);
 
+  // Manejar cambio de archivo
   const handleFileChange = (event: ChangeEvent<HTMLInputElement>) => {
     const file = event.target.files?.[0];
     if (file) {
@@ -111,20 +124,36 @@ const App: React.FC = () => {
     }
   };
 
-  const totalPages = Math.ceil(usersData.length / ITEMS_PER_PAGE);
+  // Calcular el número total de páginas
+  const totalPages = Math.ceil(
+    (showVerified
+      ? usersData.length
+      : usersData.filter((user) => !user.is_verified).length) / ITEMS_PER_PAGE
+  );
 
+  // Calcular índices de inicio y fin según la página actual
   const startIndex = (currentPage - 1) * ITEMS_PER_PAGE;
   const endIndex = startIndex + ITEMS_PER_PAGE;
 
-  const currentUsers = usersData.slice(startIndex, endIndex);
+  // Filtrar usuarios según el estado de mostrar verificados
+  const currentUsers = usersData
+    .filter((user) => (showVerified ? true : !user.is_verified))
+    .slice(startIndex, endIndex);
 
+  // Manejar cambio de página
   const handlePageChange = (page: number) => {
     setCurrentPage(page);
   };
 
+  // Manejar cambio de estado para mostrar verificados
+  const handleToggleVerified = () => {
+    setShowVerified(!showVerified);
+    setCurrentPage(1); // Reiniciar la página al cambiar la opción de mostrar verificados
+  };
+
   return (
     <div className="App">
-      <h1>User Cards App</h1>
+      <h1>IgVendettapp</h1>
       {usersData.length > 0 ? (
         <>
           {fileLoaded ? (
@@ -133,6 +162,12 @@ const App: React.FC = () => {
                 <p>¡Cargue un nuevo archivo JSON!</p>
                 <input type="file" accept=".json" onChange={handleFileChange} />
               </div>
+              <button
+                className={`toggle-verified ${showVerified ? 'active' : ''}`}
+                onClick={handleToggleVerified}
+              >
+                Mostrar verificados: {showVerified ? 'Sí' : 'No'}
+              </button>
               <div className="card-container">
                 {currentUsers.map((user) => (
                   <Card key={user.id} user={user} />
@@ -140,13 +175,19 @@ const App: React.FC = () => {
               </div>
               <div className="pagination">
                 {currentPage > 1 && (
-                  <button className="pagination-button" onClick={() => handlePageChange(currentPage - 1)}>
+                  <button
+                    className="pagination-button"
+                    onClick={() => handlePageChange(currentPage - 1)}
+                  >
                     Anterior
                   </button>
                 )}
                 <span className="page-info">{`Página ${currentPage} de ${totalPages}`}</span>
                 {currentPage < totalPages && (
-                  <button className="pagination-button" onClick={() => handlePageChange(currentPage + 1)}>
+                  <button
+                    className="pagination-button"
+                    onClick={() => handlePageChange(currentPage + 1)}
+                  >
                     Siguiente
                   </button>
                 )}
